608106c22432da3885e29bf8f8fb87220d79da62
Joe Hoshina <jhoshina@uci.edu>
2025-08-01 01:23:14 -0700

    final layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Story from "./components/Story";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <main>
+        <Hero />
+        <Story />
+        <Projects />
+        <Contact />
+      </main>
+      <Footer />
     </div>
   );
 };
